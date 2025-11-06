7e1cdef415558db0bd7de58e28bf2006cb7e95aa
Caleb C <calebbcassin@gmail.com>
2025-11-06 19:19:08 +0000

    Added additional Google HTML files for testing. Added command line input for file names. Removed .idea file from git.

--- src/carouselimageextractor.rb
+++ src/carouselimageextractor.rb
@@ -1,16 +1,7 @@
-# Switch away from using classes, instead use data-attrid="kc:/..."
-# Get Highest level kc
-# Get list of all links inside that kc
-# find image inside the link
-# get raw text, parse it by new line
-
-
-
 require 'nokogiri'
 require 'json'
 
 class CarouselImageExtractor
-
 
   @@image_id_regex = /ii=\['([^()]+?)'\]/
   @@image_data_regex = /var s='([^()]+?)'/
@@ -21,42 +12,66 @@ class CarouselImageExtractor
       puts error
     end
 
-    @images = gather_scripts
+    @images = gather_images
   end
 
+  # Uses the provided document and pre-gathered image data to create a json file of all the entries in the carousel
+  # Params:
+  # - output_file_path: the path to write the resulting JSON data to
   def extract(output_file_path)
+
+    # Carousels tend to have a data-attrid that begins with "kc:/" and tend to be the first of the matching elements to
+    # appear on the page. This may fail if another "kc:/" element precedes the expected one.
     parent_matches = @document.search('//div[contains(@data-attrid,"kc:/")][1]')
     parent = parent_matches.first
 
+    # The best way to find the type of information the carousel holds is to see which tab the user has selected
+    section_title = @document.search('//*[@role="tab"][@aria-selected="true"][1]').first.text.downcase
+
+    # Each link within the carousel holds both the image and text of each entry. There tend to be no other links within
+    # the carousel.
     links = parent.search('.//a')
 
     json_array = []
 
     links.each do |a|
       img = a.search(".//img").first
-      extensions = a.search("./div/div")
+      extensions = a.search(".//div/div")
+
+      # The first object in this array is the title, so we shift it out
       title = extensions.shift
 
+      # Some links start with "https://google...." while others do not, so we check for that here.
+      # Hidden images use a URL source while images that aren't hidden use data that we have to parse the script tags
+      # for
       info_object = {
         "name" => title.text,
         "extensions" => extensions.map {|ext| ext.text },
-        "link" => "https://www.google.com" + a['href'],
+        "link" => a["href"].start_with?('/') ? "https://www.google.com" + a['href'] : a['href'],
         "image" => img['data-src'] ? img['data-src'] : @images[img['id']],
       }
       json_array << info_object
     end
 
-    file = File.write(output_file_path, JSON.pretty_generate({"artworks" => json_array}))
+    File.open(output_file_path, 'w') do |f|
+      f.write(JSON.pretty_generate({section_title => json_array}))
+    end
   end
 
   private
-  def gather_scripts
+
+  # Loops through every script element within the html to identify the ones that contain image data
+  # Output:
+  # A JSON dictionary that uses the image ids as keys for image data
+  def gather_images
     image_object = {}
     @document.search('//script').each do |script|
       image_data = script.text.strip.match(@@image_data_regex)
       next unless image_data and image_data.size > 1
       image_id = script.text.strip.match(@@image_id_regex)
       next unless image_id and image_id.size > 1
+
+      # The second match group excludes the identifying regex (i.e. "ii=['....']")
       image_object[image_id[1]] = image_data[1].encode('utf-8')
     end
     image_object
--- src/main.rb
+++ src/main.rb
@@ -1,5 +1,46 @@
 require_relative 'carouselimageextractor'
+require 'net/http'
 
-extractor = CarouselImageExtractor.new('files/van-gogh-paintings.html')
+puts "\e[1mWelcome to the Carousel Image Extractor!\e[22m"
+puts "\nPlease input your desired input HTML file."
 
-extractor.extract('output.json')
+html_path = gets.chomp
+
+readable = File.readable?(html_path)
+html_extension = File.extname(html_path).eql?('.html') || File.extname(html_path).eql?('.htm')
+
+if readable and html_extension
+  puts "Got it! Preparing to read file at #{html_path}"
+  puts "\nPlease input your desired output JSON file."
+
+  output_path = gets.chomp
+
+  writable = File.writable?(output_path) || File.writable?(File.dirname(output_path))
+  json_extension = File.extname(output_path).eql?('.json')
+  if !writable
+    puts "'#{output_path}' is not writable or does not exist. Please try again."
+    exit(-1)
+  elsif !json_extension
+    puts "'#{output_path}' is is not a JSON file. Please try again."
+    exit(-1)
+  end
+
+  puts "Thank you! Now attempting to extract the images..."
+
+  begin
+    extractor = CarouselImageExtractor.new(html_path)
+    extractor.extract(output_path)
+    puts "Data successfully extracted to #{output_path}!"
+  rescue StandardError => e
+    puts "The following error occurred while attempting to extract the images: #{e.message}"
+  end
+elsif !readable
+  puts "'#{html_path}' either does not exist or you don't have permission to write to it. Please try again."
+else
+  puts "'#{html_path}' does not lead to an HTML file. Please try again."
+end
+
+
+
+
+
